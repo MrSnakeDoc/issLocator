ace11ceaa0967bd8a82ab1cf420f41d5a9c2c1b6
MrSnakeDoc <selabbadi@gmail.com>
2022-06-30 16:42:37 +0200

    deploy test maybe final

--- app/controllers/home.py
+++ app/controllers/home.py
@@ -3,9 +3,6 @@ from flask import send_from_directory
 from flask_restful import Resource
 
 class Home(Resource):
-    # def get(self):
-    #     return send_from_directory('public', "index.html")
     def get(self):
-        return {
-            "message": "Welcome to the ISS Locator API"
-        }
+        return send_from_directory('public', "index.html")
+        
